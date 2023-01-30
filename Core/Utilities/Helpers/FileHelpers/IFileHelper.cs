90865fdeea261a9ed0b4570bc58db2153f7c28a9
Hüseyin Gökay Benian <creatorgky@outlook.com>
2023-01-30 15:01:26 +0300

    Core Katmanı

---
+++ Core/Utilities/Helpers/FileHelpers/IFileHelper.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.FileHelpers
+{
+    public interface IFileHelper
+    {
+        string Upload(IFormFile file, string root);
+        void Delete(string root);
+        string Update(IFormFile file, string filePath, string root);
+    }
+}
